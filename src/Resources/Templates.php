088e89165a5ff9121589023d1ec97ad48347ca4c
Fábio Martins <fabiohbmartins@gmail.com>
2026-01-08 18:21:59 -0300

    Additive tab select fix

--- src/Resources/Templates.php
+++ src/Resources/Templates.php
@@ -3,6 +3,7 @@
 namespace Bildvitta\IssJuridico\Resources;
 
 use Bildvitta\IssJuridico\IssJuridico;
+use Illuminate\Http\Client\RequestException;
 
 class Templates
 {
@@ -13,10 +14,15 @@ class Templates
         $this->juridico = $juridico;
     }
 
-    public function documentType($data)
+    /**
+     * @throws RequestException
+     */
+    public function documentType($data, int $limit = 48, int $offset = 0, string $search = '')
     {
-        return $this->juridico->request->get(
-            "/templates/document-type/{$data}"
-        )->object();
+        $search = urlencode($search);
+
+        $url = "/templates/document-type/{$data}?limit={$limit}&offset={$offset}&search={$search}";
+
+        return $this->juridico->request->get($url)->throw()->object();
     }
 }
